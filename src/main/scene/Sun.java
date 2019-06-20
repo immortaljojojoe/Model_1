bb1d30daf26bc5e4082be0285987f2ac6aca8562
immortaljojojoe <38205303+immortaljojojoe@users.noreply.github.com>
2019-06-20 14:54:16 -0700

    finish a loop of sun

---
+++ src/main/scene/Sun.java
@@ -0,0 +1,31 @@
+package scene;
+
+import java.awt.*;
+
+public class Sun {
+    private int sun_radius;
+    private int x,y;
+    private Color sun_color= new Color(240,150,50);
+    public Sun(int size){
+        sun_radius =size;
+        x=-size/2;
+        y=100;
+    }
+    public int getSun_radius(){
+        return sun_radius;
+    }
+    public int x(){
+        return x;
+    }
+    public int y(){
+        return y;
+    }
+    public Color getColor(){
+        return sun_color;
+    }
+    public void refreshPlace(){
+        if(x>3700){x=-280;}
+        x++;
+    }
+
+}
--- src/main/utility/Gameplay.java
+++ src/main/utility/Gameplay.java
@@ -1,6 +1,7 @@
 package utility;
 
 import scene.Sky;
+import scene.Sun;
 import ui.MapGenerator;
 
 import javax.swing.*;
@@ -11,10 +12,12 @@ import java.awt.event.ActionListener;
 public class Gameplay extends JPanel implements ActionListener {
     private MapGenerator map;
     int width, height;
-    private int delay = 10;
+    private int delay = 1;
     private Timer timer;
     // 天空
     private Sky sky= new Sky();
+    //太阳
+    private Sun sun= new Sun(50);
 
 
     Gameplay(int width, int height) {
@@ -30,12 +33,15 @@ public class Gameplay extends JPanel implements ActionListener {
     public void paint(Graphics g) {
         g.setColor(sky.getSky_color());
         g.fillRect(0, 0, width - 1, height - 1);
+        g.setColor(sun.getColor());
+        g.fillOval(sun.x(),sun.y(),sun.getSun_radius()*2,sun.getSun_radius()*2);
 
     }
 
     public void actionPerformed(ActionEvent e) {
         timer.start();
         sky.refreshColor();
+        sun.refreshPlace();
         repaint();
     }
 }
